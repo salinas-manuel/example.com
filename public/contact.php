224514cd1385f456a470c3f209d1f93387069bf4
Manuel Salinas <root@salinas.manuel_1980>
2017-12-15 09:43:28 -0600

    Fixed contact.php

--- public/contact.php
+++ public/contact.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Contact Me - Manuel Salinas</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
-      <nav>
-          <a href="/">Home</a> |
-          <a href="resume.html">Resume</a> |
-          <a href="contact.php">Contact</a>
-      </nav>
+    <head>
+      <meta charset="UTF-8">
+      <title>Contact</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <nav>
+            <a href="/">Home</a> |
+            <a href="resume.html">Resume</a> |
+            <a href="contact.php">Contact</a>
+        </nav>
+       <h1>Contact</h1>
+    </body>
 </html>
