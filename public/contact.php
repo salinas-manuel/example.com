eacc68dc687c704473c9b945d09aaaae2f592d04
Manuel Salinas <root@salinas.manuel_1980>
2017-12-15 10:10:46 -0600

    added a basic web form

--- public/contact.php
+++ public/contact.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
       <meta charset="UTF-8">
-      <title>Contact</title>
+      <title>Contact Me - Manuel Salinas</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
@@ -11,6 +11,34 @@
             <a href="resume.html">Resume</a> |
             <a href="contact.php">Contact</a>
         </nav>
-       <h1>Contact</h1>
+       <h1 id="header" class="header">Contact</h1>
+       <form method="post">
+           <div>
+               <label for="firstName">First Name</label><br>
+               <input type="text" name="first_name" id="firstName">
+           </div>
+
+           <div>
+               <label for="lastName" id="lastName">Last Name</label><br>
+               <input type="text" name="last_name">
+           </div>
+
+           <div>
+               <label for="email" id="email">Email</label><br>
+               <input type="text" name="email">
+           </div>
+
+           <div>
+               <label for="subject" id="subject">Subject</label><br>
+               <input type="text" name="subject">
+           </div>
+
+           <div>
+               <label for="message" id="message">Message</label><br>
+               <textarea name="message"></textarea>
+           </div>
+
+           <input type="submit">
+       </form>
     </body>
 </html>
